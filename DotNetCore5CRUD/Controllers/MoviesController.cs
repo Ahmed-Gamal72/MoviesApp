9ac9a21069721fd56fe828e0b10cf20dbb440f73
Jimmy Neotron <Jimmy Neotron@DESKTOP-PTBTSDV>
2022-04-16 15:34:54 +0200

    Add project files.

---
+++ DotNetCore5CRUD/Controllers/MoviesController.cs
@@ -0,0 +1,99 @@
+﻿using DotNetCore5CRUD.Models;
+using DotNetCore5CRUD.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore5CRUD.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public MoviesController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movies.ToListAsync();
+            return View(movies);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = await _context.Genres.OrderBy(m => m.Name).ToListAsync()
+            };
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(MovieFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Genres = await _context.Genres.OrderBy(m => m.Name).ToListAsync();
+                return View(model);
+            }
+            var files = Request.Form.Files;
+            if (!files.Any())
+            {
+                model.Genres = await _context.Genres.OrderBy(m => m.Name).ToListAsync();
+                ModelState.AddModelError("Poster", "Please select movie poster!");
+                return View(model);
+            }
+
+            var poster = files.FirstOrDefault();
+            var allowedExtension = new List<string> { ".png", ".jpg" };
+            if (!allowedExtension.Contains(Path.GetExtension(poster.FileName).ToLower()))
+            {
+                model.Genres = await _context.Genres.OrderBy(m => m.Name).ToListAsync();
+                ModelState.AddModelError("Poster", "Only .JPG, .PNG images are allowed!");
+                return View(model);
+            }
+
+            if (poster.Length>1048576)
+            {
+                model.Genres = await _context.Genres.OrderBy(m => m.Name).ToListAsync();
+                ModelState.AddModelError("Poster", "Poster cannot be more than 1 MB!");
+                return View(model);
+            }
+
+            using var dataStream = new MemoryStream();
+            await poster.CopyToAsync(dataStream);
+            var movies = new Movie
+            {
+                Title = model.Title,
+                GenreId = model.GenreId,
+                Year = model.Year,
+                Rate = model.Rate,
+                StoryLine=model.StoryLine,
+                Poster=dataStream.ToArray()
+            };
+
+            _context.Movies.Add(movies);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit (int? id)
+        {
+            if (id == null)
+                return BadRequest();
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie == null)
+                return NotFound();
+
+            return View();
+                
+        }
+    }
+}
---
+++ DotNetCore5CRUD/Migrations/20220408182140_editinfieldinMovies.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNetCore5CRUD.Migrations
+{
+    public partial class editinfieldinMovies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Poster",
+                table: "Movies",
+                type: "varbinary(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Poster",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+        }
+    }
+}
